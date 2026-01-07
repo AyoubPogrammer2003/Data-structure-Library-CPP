ef91b96f920a76846aadec30d0a66be049132d59
Ayoub Programmer <dzshark7@gmail.com>
2026-01-07 14:26:19 +0100

    "add MyQueue library

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include"DS-Library/clsDblLinkedList.h"
+
+
+int main()
+{
+
+
+
+	clsDblLinkedList <int> DblLinkedList;
+	DblLinkedList.DeleteFirstNode();
+
+
+
+	return 0;
+}
