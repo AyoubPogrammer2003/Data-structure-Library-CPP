4cf8bea9594f13c879c1fb28bf4760e7feba66c4
Ayoub Programmer <dzshark7@gmail.com>
2026-01-19 14:22:11 +0100

    Add New Methods

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
-#include<iostream>
-using namespace std;
-#include"DS-Library/clsDblLinkedList.h"
 
+#include <iostream>
+#include "clsMyDynamicArray.h"
+
+using namespace std;
 
 int main()
 {
 
+    clsMyDynamicArray <int> MyDynamicArray(5);
 
+    MyDynamicArray.SetItem(0, 10);
+    MyDynamicArray.SetItem(1, 20);
+    MyDynamicArray.SetItem(2, 30);
+    MyDynamicArray.SetItem(3, 40);
+    MyDynamicArray.SetItem(4, 50);
 
-	clsDblLinkedList <int> DblLinkedList;
-	DblLinkedList.DeleteFirstNode();
+    cout << "\nIs Empty?  " << MyDynamicArray.IsEmpty();
+    cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    cout << "\nArray Items: \n";
 
+    MyDynamicArray.PrintList();
 
+    MyDynamicArray.InsertAt(2, 500);
+    cout << "\nArray Items after deleting item(2): \n";
+    cout << "\nArray Size: " << MyDynamicArray.Size() << "\n";
+    MyDynamicArray.PrintList();
 
-	return 0;
+    system("pause>0");
+
 }
